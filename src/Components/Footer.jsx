02a974a307ad035d99f3c3db91a5b594a62e089d
GerardoSuhr <gerardo.suhr@hotmail.com>
2023-04-08 00:06:50 -0300

    correccion footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,10 +7,6 @@ const Footer = () => {
     <footer className={theme.color}>
       <p>Powered by</p>
       <img src="../images/DH.png" alt="DH-logo" />
-      <img src="../assets/ico-facebook.png" alt="Facebook-logo" />
-      <img src="../images/ico-instagram.png" alt="Instagram-logo" />
-      <img src="../images/ico-tiktok.png" alt="TikTok-logo" />
-      <img src="../images/ico-whatsapp.png" alt="Whatsapp-logo" />
     </footer>
   );
 };
